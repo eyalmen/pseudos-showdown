a444d6399cde4bcd785d13c353f761c815e21125
socoolmudkip <ebtobin@gmail.com>
2024-02-19 13:55:30 -0800

    he

--- data/mods/gen9rebalanced/moves.ts
+++ data/mods/gen9rebalanced/moves.ts
@@ -4361,7 +4361,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		name: "Dual Chop",
 		pp: 15,
 		priority: 0,
-		flags: {contact: 1, protect: 1, mirror: 1, slicing: 1},
+		flags: {contact: 1, protect: 1, mirror: 1},
 		multihit: 2,
 		secondary: null,
 		target: "normal",
